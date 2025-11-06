98a80c5917b90cab99cb2e91f701262e9b76f2e0
Adam Dilger <adam.dilger@microba.com>
2025-11-07 08:17:13 +1000

    bit further

--- crates/oxc_linter/src/rules/vue/component_definition_name_casing.rs
+++ crates/oxc_linter/src/rules/vue/component_definition_name_casing.rs
@@ -1,4 +1,7 @@
-use oxc_ast::{AstKind, ast::{Expression, StringLiteral}};
+use oxc_ast::{
+    AstKind,
+    ast::{Expression, IdentifierName, StringLiteral},
+};
 use oxc_diagnostics::OxcDiagnostic;
 use oxc_macros::declare_oxc_lint;
 use oxc_span::Span;
@@ -57,129 +60,150 @@ declare_oxc_lint!(
 
 impl Rule for ComponentDefinitionNameCasing {
     fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
-      // if let AstKind::ExportDefaultDeclaration(object_property) = node.kind() {
-      //   println!("Found export default declaration at {:?}", object_property.span);
-      // }
+        if let AstKind::ObjectProperty(object_property) = node.kind() {
+            if !object_property.key.is_specific_static_name("name") {
+                return;
+            }
 
-      let AstKind::ObjectProperty(object_property) = node.kind() else {
-        return;
-      };
+            let Expression::StringLiteral(string_literal) = &object_property.value else { return };
 
-      if !object_property.key.is_specific_static_name("name") {
-        return
-      }
+            println!("Found component name: {}", string_literal.value);
 
-      let Expression::StringLiteral(string_literal) = &object_property.value else {
-        return
-      };
+            let identifier_name: &StringLiteral = string_literal;
+            check_identifier(identifier_name, ctx);
+        } else if let AstKind::CallExpression(call_expression) = node.kind() {
+            let callee = &call_expression.callee;
 
-      println!("Found component name: {}", string_literal.value);
+            let Expression::StaticMemberExpression(static_member_expression) = callee else {
+                return;
+            };
 
-      // if the first character is not a capital letter between A-Z, report diagnostic
-      let identifier_name: &StringLiteral = string_literal;
+            let IdentifierName { span, name } = &static_member_expression.property;
+            if name != "component" {
+                return;
+            }
 
-      if has_symbols(&identifier_name.value) {
+            println!("Found static member expression property: {:?}", span);
+            println!("Found method name: {}", name);
+
+            if call_expression.arguments.is_empty() {
+                return;
+            }
+
+            let first_arg = &call_expression.arguments[0];
+            let Some(Expression::StringLiteral(identifier_name)) = &first_arg.as_expression()
+            else {
+                return;
+            };
+
+            println!("Found first argument: {:?}", identifier_name.value);
+            check_identifier(identifier_name, ctx);
+        }
+    }
+}
+
+fn check_identifier(identifier_name: &StringLiteral, ctx: &LintContext) {
+    if has_symbols(&identifier_name.value) {
         ctx.diagnostic(component_definition_name_casing_diagnostic(identifier_name.span));
-        return
-      }
+        return;
+    }
 
-      let is_kebab_case = is_kebab_case(&identifier_name.value);
-      if is_kebab_case {
+    let is_kebab_case = is_kebab_case(&identifier_name.value);
+    if is_kebab_case {
         ctx.diagnostic_with_fix(
-          component_definition_name_casing_diagnostic(identifier_name.span),
-           |fixer| {
-            fixer.replace(identifier_name.span.shrink(1), kebab_to_pascal(&identifier_name.value))
-          }
+            component_definition_name_casing_diagnostic(identifier_name.span),
+            |fixer| {
+                fixer.replace(
+                    identifier_name.span.shrink(1),
+                    kebab_to_pascal(&identifier_name.value),
+                )
+            },
         );
-      }
     }
 }
 
 fn has_symbols(str: &str) -> bool {
-  for c in str.chars() {
-    if !c.is_ascii_alphabetic() && c != '-' {
-      return true
+    for c in str.chars() {
+        if !c.is_ascii_alphabetic() && c != '-' {
+            return true;
+        }
     }
-  }
-  false
+    false
 }
 
-
-fn is_kebab_case(str: &str) -> bool {
-  fn has_upper(str: &str) -> bool {
+fn has_upper(str: &str) -> bool {
     for c in str.chars() {
-      if c.is_uppercase() {
-        return true
-      }
+        if c.is_uppercase() {
+            return true;
+        }
     }
     false
-  }
+}
 
-  fn starts_with_hyphen(str: &str) -> bool {
+fn starts_with_hyphen(str: &str) -> bool {
     if let Some(first_char) = str.chars().next() {
-      return first_char == '-'
+        return first_char == '-';
     }
     false
-  }
+}
 
-  fn has_consecutive_hyphens(str: &str) -> bool {
+fn has_consecutive_hyphens(str: &str) -> bool {
     let mut prev_was_hyphen = false;
     for c in str.chars() {
-      if c == '-' {
-        if prev_was_hyphen {
-          return true
+        if c == '-' {
+            if prev_was_hyphen {
+                return true;
+            }
+            prev_was_hyphen = true;
+        } else {
+            prev_was_hyphen = false;
         }
-        prev_was_hyphen = true;
-      } else {
-        prev_was_hyphen = false;
-      }
     }
     false
-  }
+}
 
-  fn has_underscore_or_whitespace(str: &str) -> bool {
+fn has_underscore_or_whitespace(str: &str) -> bool {
     for c in str.chars() {
-      if c == '_' || c.is_whitespace() {
-        return true
-      }
+        if c == '_' || c.is_whitespace() {
+            return true;
+        }
     }
     false
-  }
+}
 
-  return
-    !has_upper(str) &&
+fn is_kebab_case(str: &str) -> bool {
+    return !has_upper(str) &&
     !has_symbols(str) &&
     !starts_with_hyphen(str) && // starts with hyphen is not kebab-case
     !has_consecutive_hyphens(str) && // consecutive hyphens is not kebab-case
-    !has_underscore_or_whitespace(str) // underscore or whitespace is not kebab-case
+    !has_underscore_or_whitespace(str); // underscore or whitespace is not kebab-case
 }
 
 fn kebab_to_pascal(check_name: &str) -> String {
-  let mut result = String::new();
-  let mut capitalize_next = true;
+    let mut result = String::new();
+    let mut capitalize_next = true;
 
-  for c in check_name.chars() {
-      if c == '-' {
-          capitalize_next = true;
-          continue
-      }
+    for c in check_name.chars() {
+        if c == '-' {
+            capitalize_next = true;
+            continue;
+        }
 
-      if capitalize_next {
-          result.push(c.to_ascii_uppercase());
-          capitalize_next = false;
-      } else {
-          result.push(c);
-      }
-  }
+        if capitalize_next {
+            result.push(c.to_ascii_uppercase());
+            capitalize_next = false;
+        } else {
+            result.push(c);
+        }
+    }
 
-  result
+    result
 }
 
 #[test]
 fn test() {
     use crate::tester::Tester;
     use std::path::PathBuf;
-
 
     let pass = vec![
         (
@@ -244,66 +268,66 @@ fn test() {
             None,
             Some(PathBuf::from("test.vue")),
         ), // languageOptions,
-        // (
-        //     "Vue.component(fooBar, {})",
-        //     Some(serde_json::json!(["kebab-case"])),
-        //     None,
-        //     Some(PathBuf::from("test.vue")),
-        // ), // languageOptions,
-        // ("Vue.component('FooBar', component)", None, None, Some(PathBuf::from("test.vue"))), // languageOptions,
-        // (
-        //     "Vue.component('FooBar', component)",
-        //     Some(serde_json::json!(["PascalCase"])),
-        //     None,
-        //     Some(PathBuf::from("test.vue")),
-        // ), // languageOptions,
-        // (
-        //     "Vue.component('foo-bar', component)",
-        //     Some(serde_json::json!(["kebab-case"])),
-        //     None,
-        //     Some(PathBuf::from("test.vue")),
-        // ), // languageOptions,
-        // (
-        //     "Vue.component(fooBar, component)",
-        //     Some(serde_json::json!(["kebab-case"])),
-        //     None,
-        //     Some(PathBuf::from("test.vue")),
-        // ), // languageOptions,
-        // (
-        //     "app.component('FooBar', component)",
-        //     Some(serde_json::json!(["PascalCase"])),
-        //     None,
-        //     Some(PathBuf::from("test.vue")),
-        // ), // languageOptions,
-        // ("Vue.mixin({})", None, None, Some(PathBuf::from("test.vue"))), // languageOptions,
-        // ("foo({})", None, None, Some(PathBuf::from("test.vue"))),       // languageOptions,
-        // ("foo('foo-bar', {})", None, None, Some(PathBuf::from("test.vue"))), // languageOptions,
-        // (
-        //     "Vue.component(`fooBar${foo}`, component)",
-        //     Some(serde_json::json!(["kebab-case"])),
-        //     None,
-        //     Some(PathBuf::from("test.vue")),
-        // ), // languageOptions,
-        // (
-        //     "app.component(`fooBar${foo}`, component)",
-        //     Some(serde_json::json!(["kebab-case"])),
-        //     None,
-        //     Some(PathBuf::from("test.vue")),
-        // ), // languageOptions,
-        // ("fn1(component.data)", None, None, Some(PathBuf::from("test.js"))), // languageOptions,
-        // ("<script setup> defineOptions({}) </script>", None, None, Some(PathBuf::from("test.vue"))), // {        "parser": require("vue-eslint-parser"),        ...languageOptions      },
-        // (
-        //     "<script setup> defineOptions({name: 'FooBar'}) </script>",
-        //     Some(serde_json::json!(["PascalCase"])),
-        //     None,
-        //     Some(PathBuf::from("test.vue")),
-        // ), // {        "parser": require("vue-eslint-parser"),        ...languageOptions      },
-        // (
-        //     "<script setup> defineOptions({name: 'foo-bar'}) </script>",
-        //     Some(serde_json::json!(["kebab-case"])),
-        //     None,
-        //     Some(PathBuf::from("test.vue")),
-        // ), // {        "parser": require("vue-eslint-parser"),        ...languageOptions      }
+        (
+            "Vue.component(fooBar, {})",
+            Some(serde_json::json!(["kebab-case"])),
+            None,
+            Some(PathBuf::from("test.vue")),
+        ), // languageOptions,
+        ("Vue.component('FooBar', component)", None, None, Some(PathBuf::from("test.vue"))), // languageOptions,
+        (
+            "Vue.component('FooBar', component)",
+            Some(serde_json::json!(["PascalCase"])),
+            None,
+            Some(PathBuf::from("test.vue")),
+        ), // languageOptions,
+        (
+            "Vue.component('foo-bar', component)",
+            Some(serde_json::json!(["kebab-case"])),
+            None,
+            Some(PathBuf::from("test.vue")),
+        ), // languageOptions,
+        (
+            "Vue.component(fooBar, component)",
+            Some(serde_json::json!(["kebab-case"])),
+            None,
+            Some(PathBuf::from("test.vue")),
+        ), // languageOptions,
+        (
+            "app.component('FooBar', component)",
+            Some(serde_json::json!(["PascalCase"])),
+            None,
+            Some(PathBuf::from("test.vue")),
+        ), // languageOptions,
+        ("Vue.mixin({})", None, None, Some(PathBuf::from("test.vue"))), // languageOptions,
+        ("foo({})", None, None, Some(PathBuf::from("test.vue"))),       // languageOptions,
+        ("foo('foo-bar', {})", None, None, Some(PathBuf::from("test.vue"))), // languageOptions,
+        (
+            "Vue.component(`fooBar${foo}`, component)",
+            Some(serde_json::json!(["kebab-case"])),
+            None,
+            Some(PathBuf::from("test.vue")),
+        ), // languageOptions,
+        (
+            "app.component(`fooBar${foo}`, component)",
+            Some(serde_json::json!(["kebab-case"])),
+            None,
+            Some(PathBuf::from("test.vue")),
+        ), // languageOptions,
+        ("fn1(component.data)", None, None, Some(PathBuf::from("test.js"))), // languageOptions,
+        ("<script setup> defineOptions({}) </script>", None, None, Some(PathBuf::from("test.vue"))), // {        "parser": require("vue-eslint-parser"),        ...languageOptions      },
+        (
+            "<script setup> defineOptions({name: 'FooBar'}) </script>",
+            Some(serde_json::json!(["PascalCase"])),
+            None,
+            Some(PathBuf::from("test.vue")),
+        ), // {        "parser": require("vue-eslint-parser"),        ...languageOptions      },
+        (
+            "<script setup> defineOptions({name: 'foo-bar'}) </script>",
+            Some(serde_json::json!(["kebab-case"])),
+            None,
+            Some(PathBuf::from("test.vue")),
+        ), // {        "parser": require("vue-eslint-parser"),        ...languageOptions      }
     ];
 
     let fail = vec![
@@ -405,30 +429,30 @@ fn test() {
             None,
             Some(PathBuf::from("test.js")),
         ), // languageOptions,
-        // (
-        //     "Vue.component('foo_bar', {})",
-        //     Some(serde_json::json!(["kebab-case"])),
-        //     None,
-        //     Some(PathBuf::from("test.vue")),
-        // ), // languageOptions,
-        // (
-        //     "Vue.component(`foo_bar`, {})",
-        //     Some(serde_json::json!(["kebab-case"])),
-        //     None,
-        //     Some(PathBuf::from("test.vue")),
-        // ), // languageOptions,
-        // (
-        //     "<script setup> defineOptions({name: 'foo-bar'}) </script>",
-        //     Some(serde_json::json!(["PascalCase"])),
-        //     None,
-        //     Some(PathBuf::from("test.vue")),
-        // ), // {        "parser": require("vue-eslint-parser"),        ...languageOptions      },
-        // (
-        //     "<script setup> defineOptions({name: 'FooBar'}) </script>",
-        //     Some(serde_json::json!(["kebab-case"])),
-        //     None,
-        //     Some(PathBuf::from("test.vue")),
-        // ), // {        "parser": require("vue-eslint-parser"),        ...languageOptions      }
+                                                                                      // (
+                                                                                      //     "Vue.component('foo_bar', {})",
+                                                                                      //     Some(serde_json::json!(["kebab-case"])),
+                                                                                      //     None,
+                                                                                      //     Some(PathBuf::from("test.vue")),
+                                                                                      // ), // languageOptions,
+                                                                                      // (
+                                                                                      //     "Vue.component(`foo_bar`, {})",
+                                                                                      //     Some(serde_json::json!(["kebab-case"])),
+                                                                                      //     None,
+                                                                                      //     Some(PathBuf::from("test.vue")),
+                                                                                      // ), // languageOptions,
+                                                                                      // (
+                                                                                      //     "<script setup> defineOptions({name: 'foo-bar'}) </script>",
+                                                                                      //     Some(serde_json::json!(["PascalCase"])),
+                                                                                      //     None,
+                                                                                      //     Some(PathBuf::from("test.vue")),
+                                                                                      // ), // {        "parser": require("vue-eslint-parser"),        ...languageOptions      },
+                                                                                      // (
+                                                                                      //     "<script setup> defineOptions({name: 'FooBar'}) </script>",
+                                                                                      //     Some(serde_json::json!(["kebab-case"])),
+                                                                                      //     None,
+                                                                                      //     Some(PathBuf::from("test.vue")),
+                                                                                      // ), // {        "parser": require("vue-eslint-parser"),        ...languageOptions      }
     ];
 
     let fix = vec![
@@ -447,41 +471,41 @@ fn test() {
         ),
         // (
         //     "
-			  //       export default {
-			  //         name: 'foo_bar'
-			  //       }
-			  //     ",
+        //       export default {
+        //         name: 'foo_bar'
+        //       }
+        //     ",
         //     "
-			  //       export default {
-			  //         name: 'FooBar'
-			  //       }
-			  //     ",
+        //       export default {
+        //         name: 'FooBar'
+        //       }
+        //     ",
         //     None,
         // ),
         // (
         //     "
-			  //       export default {
-			  //         name: 'foo_bar'
-			  //       }
-			  //     ",
+        //       export default {
+        //         name: 'foo_bar'
+        //       }
+        //     ",
         //     "
-			  //       export default {
-			  //         name: 'FooBar'
-			  //       }
-			  //     ",
+        //       export default {
+        //         name: 'FooBar'
+        //       }
+        //     ",
         //     Some(serde_json::json!(["PascalCase"])),
         // ),
         // (
         //     "
-			  //       export default {
-			  //         name: 'foo_bar'
-			  //       }
-			  //     ",
+        //       export default {
+        //         name: 'foo_bar'
+        //       }
+        //     ",
         //     "
-			  //       export default {
-			  //         name: 'foo-bar'
-			  //       }
-			  //     ",
+        //       export default {
+        //         name: 'foo-bar'
+        //       }
+        //     ",
         //     Some(serde_json::json!(["kebab-case"])),
         // ),
         // ("Vue.component('foo-bar', component)", "Vue.component('FooBar', component)", None),
